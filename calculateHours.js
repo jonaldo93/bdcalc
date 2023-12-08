9e271b81a67aab26b5c90379fca72c094fea7c19
jonaldo93 <56759761+jonaldo93@users.noreply.github.com>
2023-12-08 15:01:31 +0100

    Update calculateHours.js

--- calculateHours.js
+++ calculateHours.js
@@ -1,4 +1,3 @@
-
 // Define the base rates
 const rates = {
     MoDo: { BD: 11.75, Rate25: 5, Rate40: 4 },
@@ -8,6 +7,7 @@ const rates = {
     Feiertag: { BD: 18, Rate25: 5, Rate40: 4, Feiertag35: 4.25, Feiertag25: 10.25 }
 };
 
+// Function to calculate hours and bonuses
 function calculateHours(inputDays) {
     let output = {
         BD: 0,
@@ -17,79 +17,85 @@ function calculateHours(inputDays) {
         SonntagBD: 0,
         FeiertagAktiv35: 0,
         Feiertag25: 0,
-        ZusatzBonus: 0,
-        // Additional output lines for on-call bonuses
-        BDPlus1: 0,
-        BDPlus2: 0,
-        BDPlus3: 0,
-        BDPlus4: 0,
-        BDPlus5: 0
+        // Adding additional on-call bonuses
+        AdditionalOnCallBonuses: []
     };
 
     for (const dayType in inputDays) {
         const days = inputDays[dayType];
         const rate = rates[dayType];
 
-        if (rate && dayType !== 'fifthOnCall' && dayType !== 'sixthOnCall' && dayType !== 'seventhOnCall' && dayType !== 'eighthOnCall' && dayType !== 'ninthOnCall') {
+        if (rate) {
             output.BD += days * rate.BD;
             output.Nacht25 += days * rate.Rate25;
             output.Nacht40 += days * rate.Rate40;
-            // ... other calculations ...
+
+            if (dayType === 'Sa') {
+                output.Sonntag += days * rate.SamstagExtra;
+                output.SonntagBD += days * rate.SamstagBD25;
+            }
+
+            if (dayType === 'So') {
+                output.Sonntag += days * rate.Sonntag;
+                output.SonntagBD += days * rate.SonntagBD;
+            }
+
+            if (dayType === 'Feiertag') {
+                output.FeiertagAktiv35 += days * rate.Feiertag35;
+                output.Feiertag25 += days * rate.Feiertag25;
+            }
         }
     }
 
-    // Calculate the additional on-call bonuses
-    const onCallBonuses = [
-        { id: 'fifthOnCall', factor: 0.2 },
-        { id: 'sixthOnCall', factor: 0.4 },
-        { id: 'seventhOnCall', factor: 0.6 },
-        { id: 'eighthOnCall', factor: 0.8 },
-        { id: 'ninthOnCall', factor: 1.0 }
-    ];
-
-    onCallBonuses.forEach((bonus, index) => {
-        const dayType = inputDays[bonus.id];
-        if (dayType && rates[dayType]) {
-            const bonusHours = rates[dayType].BD * bonus.factor;
-            output[`BDPlus${index + 1}`] = bonusHours;
+    // Calculate additional on-call bonuses
+    const additionalOnCallFactors = [0.2, 0.4, 0.6, 0.8, 1.0];
+    additionalOnCallFactors.forEach((factor, index) => {
+        const onCallDay = inputDays[`additionalOnCall${index + 1}`];
+        if (onCallDay && rates[onCallDay]) {
+            output.AdditionalOnCallBonuses.push({
+                bonusType: `BD+${index + 1}`,
+                value: rates[onCallDay].BD * factor
+            });
         }
     });
 
     return output;
 }
 
+// Event listener for form submission
 document.getElementById('hoursForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Standard day inputs
     const MoDo = parseInt(document.getElementById('MoDo').value, 10) || 0;
     const Fr = parseInt(document.getElementById('Fr').value, 10) || 0;
     const Sa = parseInt(document.getElementById('Sa').value, 10) || 0;
     const So = parseInt(document.getElementById('So').value, 10) || 0;
     const Feiertag = parseInt(document.getElementById('Feiertag').value, 10) || 0;
 
-    // Additional on-calls
-    const fifthOnCall = document.getElementById('fifthOnCall').value;
-    const sixthOnCall = document.getElementById('sixthOnCall').value;
-    const seventhOnCall = document.getElementById('seventhOnCall').value;
-    const eighthOnCall = document.getElementById('eighthOnCall').value;
-    const ninthOnCall = document.getElementById('ninthOnCall').value;
+    // Include additional on-call days in the input
+    const additionalOnCalls = {};
+    for (let i = 1; i <= 5; i++) {
+        additionalOnCalls[`additionalOnCall${i}`] = document.getElementById(`additionalOnCall${i}`).value;
+    }
 
-    // Calculate the hours
-    const result = calculateHours({ MoDo, Fr, Sa, So, Feiertag, fifthOnCall, sixthOnCall, seventhOnCall, eighthOnCall, ninthOnCall });
+    const result = calculateHours({ MoDo, Fr, Sa, So, Feiertag, ...additionalOnCalls });
 
-    // Display the results
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = `
+    let resultsHTML = `
         <table style="width:100%; border-collapse: collapse;">
             <tr style="background-color: #f2f2f2;"><td>BD</td><td>${result.BD.toFixed(2)}</td></tr>
             <tr><td>Nacht 25%</td><td>${result.Nacht25.toFixed(2)}</td></tr>
             <tr style="background-color: #f2f2f2;"><td>Nacht 40%</td><td>${result.Nacht40.toFixed(2)}</td></tr>
             <tr><td>Sonntag</td><td>${result.Sonntag.toFixed(2)}</td></tr>
             <tr style="background-color: #f2f2f2;"><td>Sonntag BD</td><td>${result.SonntagBD.toFixed(2)}</td></tr>
-            <td>Feiertag aktiv 35%</td><td>${result.FeiertagAktiv35.toFixed(2)}</td></tr>
+            <tr><td>Feiertag aktiv 35%</td><td>${result.FeiertagAktiv35.toFixed(2)}</td></tr>
             <tr style="background-color: #f2f2f2;"><td>Feiertag 25%</td><td>${result.Feiertag25.toFixed(2)}</td></tr>
-            <tr><td>Zusätzlicher Bonus</td><td>${result.ZusatzBonus.toFixed(2)}</td></tr>
-        </table>
-    `;
+        `;
+
+    // Add additional on-call bonuses to the results
+    result.AdditionalOnCallBonuses.forEach(bonus => {
+        resultsHTML += `<tr><td>${bonus.bonusType}</td><td>${bonus.value.toFixed(2)}</td></tr>`;
+    });
+
+    resultsHTML += `</table>`;
+    document.getElementById('results').innerHTML = resultsHTML;
 });
