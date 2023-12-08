5a3036587a825e62bdfdde0f1b3ff0b9d2e2e90f
jonaldo93 <56759761+jonaldo93@users.noreply.github.com>
2023-12-08 16:35:43 +0100

    Update calculateHours.js

--- calculateHours.js
+++ calculateHours.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Event listener for showing/hiding additional on-call fields
     document.getElementById('totalBDs').addEventListener('change', function() {
         var totalBDs = parseInt(this.value);
 
         // Hide all additional on-call fields initially
         for (let i = 1; i <= 5; i++) {
-            document.getElementById(`additionalOnCall${i}`).style.display = 'none';
+            document.getElementById(`additionalOnCall${i}Container`).style.display = 'none';
         }
 
         // Show the required number of additional on-call fields
         for (let i = 1; i <= totalBDs - 4; i++) {
-            document.getElementById(`additionalOnCall${i}`).style.display = 'block';
+            document.getElementById(`additionalOnCall${i}Container`).style.display = 'block';
         }
     });
+
     document.getElementById('shortNoticeYesNo').addEventListener('change', function() {
         if (this.value === 'Ja') {
             document.getElementById('shortNoticeDetails').style.display = 'block';
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
 
 
 
