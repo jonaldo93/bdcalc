e7dc5610b5b8a65f8f455b3c65d8ca092b698004
jonaldo93 <56759761+jonaldo93@users.noreply.github.com>
2023-12-10 22:51:35 +0100

    Update calculateHours.js

--- calculateHours.js
+++ calculateHours.js
@@ -1,4 +1,10 @@
 function updateDiensteAndVisibility() {
+    // Check if elements exist
+    if (!document.getElementById('MoDo') || !document.getElementById('totalBDsDisplay')) {
+        console.error('Required elements are missing');
+        return;
+    }
+    
     // Calculate the total number of Dienste
     const MoDo = parseInt(document.getElementById('MoDo').value);
     const Fr = parseInt(document.getElementById('Fr').value);
@@ -6,6 +12,18 @@ function updateDiensteAndVisibility() {
     const So = parseInt(document.getElementById('So').value);
     const Feiertag = parseInt(document.getElementById('Feiertag').value);
     const totalBDs = MoDo + Fr + Sa + So + Feiertag;
+
+    // Debugging: Log the values
+    // console.log(`MoDo: ${MoDo}, Fr: ${Fr}, Sa: ${Sa}, So: ${So}, Feiertag: ${Feiertag}, Total BDs: ${totalBDs}`);
+
+    // Check if totalBDs exceeds 9
+    if (totalBDs > 9) {
+        document.getElementById('errorDisplay').textContent = 'Der Rechner ist aktuell auf eine maximale Anzahl von 9 BDs programmiert. Bitte Reset und Neueingabe.';
+        document.getElementById('errorDisplayContainer').style.display = 'block';
+        return; // Optionally, you can stop further execution
+    } else {
+        document.getElementById('errorDisplayContainer').style.display = 'none';
+    }
 
     // Update the display for total Dienste
     document.getElementById('totalBDsDisplay').textContent = 'Total Dienste: ' + totalBDs;
@@ -19,24 +37,60 @@ function updateDiensteAndVisibility() {
             container.style.display = 'none';
         }
     }
-    // Event listeners for Dienste dropdown changes
+    }
+
+    // Set up event listeners outside the function
     ['MoDo', 'Fr', 'Sa', 'So', 'Feiertag'].forEach(id => {
         document.getElementById(id).addEventListener('change', updateDiensteAndVisibility);
     });
 
-    // Initialize the display and visibility on page load
-    updateDiensteAndVisibility();
 
-    // Event listener for short notice work
-    document.getElementById('shortNoticeYesNo').addEventListener('change', function() {
-        if (this.value === 'Ja') {
-            document.getElementById('shortNoticeDetails').style.display = 'block';
-        } else {
-            document.getElementById('shortNoticeDetails').style.display = 'none';
-        }
-    });
+// Event listener for short notice work
+document.getElementById('shortNoticeYesNo').addEventListener('change', function() {
+    const shortNoticeAnswer = this.value;
+    if (shortNoticeAnswer === 'Ja') {
+        document.getElementById('labelNumFlexDienste').style.display = 'block';
+        document.getElementById('numFlexDienste').style.display = 'block';
+    } else {
+        document.getElementById('labelNumFlexDienste').style.display = 'none';
+        document.getElementById('numFlexDienste').style.display = 'none';
+        document.getElementById('flexDiensteContainer').innerHTML = '';
+        document.getElementById('flexDiensteContainer').style.display = 'none';
+    }
 });
 
+// Event listener for number of Flex Dienste
+document.getElementById('numFlexDienste').addEventListener('change', function() {
+    const numFlexDienste = parseInt(this.value);
+    const flexDiensteContainer = document.getElementById('flexDiensteContainer');
+    flexDiensteContainer.innerHTML = ''; // Clear previous entries
+
+    for (let i = 1; i <= numFlexDienste; i++) {
+        const flexDienstHtml = `
+            <div class="flexDienstRow">
+                <label>${i}. Flex Dienst - Wie kurzfristig?</label>
+                <select id="flexDienstDuration${i}">
+                    <option value="FlexGreater96">>96h vorher</option>
+                    <option value="Flex48To96">48-96h vorher</option>
+                    <option value="FlexLess48"><48h vorher</option>
+                </select>
+                <label>An welchem Tag?</label>
+                <select id="flexDienstDay${i}">
+                    <option value="MoDo">Montag-Donnerstag</option>
+                    <option value="Fr">Freitag</option>
+                    <option value="Sa">Samstag</option>
+                    <option value="So">Sonntag</option>
+                    <option value="Feiertag">Feiertag</option>
+                </select>
+            </div>`;
+        flexDiensteContainer.innerHTML += flexDienstHtml;
+    }
+
+    flexDiensteContainer.style.display = numFlexDienste > 0 ? 'block' : 'none';
+});
+
+// Initialize the display and visibility on page load
+updateDiensteAndVisibility();
 
 // Event listener for reset button
 document.getElementById('resetButton').addEventListener('click', function(event) {
@@ -53,8 +107,24 @@ document.getElementById('resetButton').addEventListener('click', function(event)
         document.getElementById(`additionalOnCall${i}Container`).style.display = 'none';
     }
 
+    // Reset and hide Flex Dienste fields
+    document.getElementById('shortNoticeYesNo').value = 'Nein';
+    document.getElementById('labelNumFlexDienste').style.display = 'none';
+    document.getElementById('numFlexDienste').style.display = 'none';
+    document.getElementById('numFlexDienste').value = '0';
+    document.getElementById('flexDiensteContainer').innerHTML = '';
+    document.getElementById('flexDiensteContainer').style.display = 'none';
+
+
     // Clear the results div
     document.getElementById('results').innerHTML = '';
+
+    // Hide and reset the error message
+    document.getElementById('errorDisplayContainer').style.display = 'none';
+    document.getElementById('errorDisplay').textC
+
+    // Recalculate total BDs
+    updateDiensteAndVisibility();
 
     event.preventDefault();
 });
@@ -62,11 +132,11 @@ document.getElementById('resetButton').addEventListener('click', function(event)
 
 // Define the base rates
 const rates = {
-    MoDo: { BD: 11.75, Rate25: 5, Rate40: 4 },
-    Fr: { BD: 13.25, Rate25: 5, Rate40: 4 },
-    Sa: { BD: 19, Rate25: 5, Rate40: 4, SamstagExtra: 0.5, SamstagBD25: 8.75 },
-    So: { BD: 18, Rate25: 5, Rate40: 4, Sonntag: 4.75, SonntagBD: 10.25 },
-    Feiertag: { BD: 18, Rate25: 5, Rate40: 4, Feiertag35: 4.25, Feiertag25: 10.25 }
+    MoDo: { BD: 11.75, Rate25: 5, Rate40: 4, TotalTime: 11.75 + 8 },
+    Fr: { BD: 13.25, Rate25: 5, Rate40: 4, TotalTime: 13.25 + 8 },
+    Sa: { BD: 19, Rate25: 5, Rate40: 4, SamstagExtra: 0.5, SamstagBD25: 8.75, TotalTime: 19 + 5.25 },
+    So: { BD: 18, Rate25: 5, Rate40: 4, Sonntag: 4.75, SonntagBD: 10.25, TotalTime: 18 + 5.25 },
+    Feiertag: { BD: 18, Rate25: 5, Rate40: 4, Feiertag35: 4.25, Feiertag25: 10.25, TotalTime: 18 + 5.25 }
 };
 
 // Function to calculate hours and bonuses
@@ -79,7 +149,8 @@ function calculateHours(inputDays) {
         SonntagBD: 0,
         FeiertagAktiv35: 0,
         Feiertag25: 0,
-        AdditionalOnCallBonuses: []
+        AdditionalOnCallBonuses: [],
+        FlexDiensteResults: []
     };
 
     for (const dayType in inputDays) {
@@ -124,61 +195,72 @@ function calculateHours(inputDays) {
         }
     });
     
-    // Check for short notice work
-    console.log('shortNoticeYesNo Element:', document.getElementById('shortNoticeYesNo'));
+    
+    // Handling Flex Dienste
     const shortNoticeAnswer = document.getElementById('shortNoticeYesNo').value;
-    
-    if (shortNoticeAnswer === 'Ja') {
-        console.log('shortNoticeDuration Element:', document.getElementById('shortNoticeDuration'));
-        const shortNoticeDuration = document.getElementById('shortNoticeDuration').value;
-    
-        console.log('shortNoticeDay Element:', document.getElementById('shortNoticeDay'));
-        const shortNoticeDay = document.getElementById('shortNoticeDay').value;
-       
-        let multiplier = 0;
-        switch (shortNoticeDuration) {
-            case 'FlexGreater96':
-                multiplier = 0.05;
-                break;
-            case 'Flex48To96':
-                multiplier = 0.15;
-                break;
-            case 'FlexLess48':
-                multiplier = 0.30;
-                break;
-        }
-    
-        const dayHours = rates[shortNoticeDay] ? rates[shortNoticeDay].BD : 0;
-        output.ShortNoticeBonus = dayHours * multiplier;
-    }
-    
 
+    if (shortNoticeAnswer === 'Ja') {
+        const numFlexDienste = parseInt(document.getElementById('numFlexDienste').value, 10);
+        let flexDienste = [];
+
+        for (let i = 1; i <= numFlexDienste; i++) {
+            const flexDuration = document.getElementById(`flexDienstDuration${i}`).value;
+            const flexDay = document.getElementById(`flexDienstDay${i}`).value;
+            let multiplier = 0;
+
+            switch (flexDuration) {
+                case 'FlexGreater96':
+                    multiplier = 0.05;
+                    break;
+                case 'Flex48To96':
+                    multiplier = 0.15;
+                    break;
+                case 'FlexLess48':
+                    multiplier = 0.3;
+                    break;
+            }
+
+            const rate = rates[flexDay];
+            const totalHours = rate ? rate.TotalTime : 0;
+            const result = totalHours * multiplier;
+
+            flexDienste.push({
+                name: `FLEX ${i}`,
+                factor: multiplier,
+                totalHours: totalHours,
+                result: result
+            });
+        }
+
+        output.FlexDiensteResults = flexDienste;
+    }
     return output;
+
 }
 
 // Event listener for form submission
 document.getElementById('hoursForm').addEventListener('submit', function(event) {
     event.preventDefault();
     // Log and check each element before accessing its value
-    console.log('MoDo Element:', document.getElementById('MoDo'));
+    // console.log('MoDo Element:', document.getElementById('MoDo'));
     const MoDo = parseInt(document.getElementById('MoDo').value, 10) || 0;
 
-    console.log('Fr Element:', document.getElementById('Fr'));
+    // console.log('Fr Element:', document.getElementById('Fr'));
     const Fr = parseInt(document.getElementById('Fr').value, 10) || 0;
 
-    console.log('Sa Element:', document.getElementById('Sa'));
+    // console.log('Sa Element:', document.getElementById('Sa'));
     const Sa = parseInt(document.getElementById('Sa').value, 10) || 0;
 
-    console.log('So Element:', document.getElementById('So'));
+    // console.log('So Element:', document.getElementById('So'));
     const So = parseInt(document.getElementById('So').value, 10) || 0;
 
-    console.log('Feiertag Element:', document.getElementById('Feiertag'));
+    // console.log('Feiertag Element:', document.getElementById('Feiertag'));
     const Feiertag = parseInt(document.getElementById('Feiertag').value, 10) || 0;
 
     // Include additional on-call days in the input   
     const additionalOnCalls = {};
     for (let i = 1; i <= 5; i++) {
-        console.log(`additionalOnCallSelect${i} Element:`, document.getElementById(`additionalOnCallSelect${i}`));
+        // console.log(`additionalOnCallSelect${i} Element:`, document.getElementById(`additionalOnCallSelect${i}`));
         additionalOnCalls[`additionalOnCallSelect${i}`] = document.getElementById(`additionalOnCallSelect${i}`).value;
     }
 
@@ -194,8 +276,8 @@ document.getElementById('hoursForm').addEventListener('submit', function(event) 
     event.preventDefault();
 
     // Log the elements to check if they are correctly selected
-    console.log(document.getElementById('MoDo'));
-    console.log(document.getElementById('additionalOnCallSelect1'));
+    // console.log(document.getElementById('MoDo'));
+    // console.log(document.getElementById('additionalOnCallSelect1'));
     // Add similar console logs for other elements
     
     const MoDo = parseInt(document.getElementById('MoDo').value, 10) || 0;
@@ -205,7 +287,7 @@ document.getElementById('hoursForm').addEventListener('submit', function(event) 
     const Feiertag = parseInt(document.getElementById('Feiertag').value, 10) || 0;
 
 
-    console.log(document.getElementById('shortNoticeDay'));
+    // console.log(document.getElementById('shortNoticeDay'));
     
     // Include additional on-call days in the input
     // Include additional on-call days in the input
@@ -237,10 +319,17 @@ document.getElementById('hoursForm').addEventListener('submit', function(event) 
         resultsHTML += `<tr><td>${bonus.bonusType}</td><td>${bonus.value.toFixed(2)}</td></tr>`;
     });
 
-    // Add short notice bonus to the results
-    if (result.ShortNoticeBonus !== undefined) {
-        resultsHTML += `<tr><td>Kurzfristig Bonus</td><td>${result.ShortNoticeBonus.toFixed(2)}</td></tr>`;
-    }
+    // Adding Flex Dienste results to the HTML
+    result.FlexDiensteResults.forEach(flexDienst => {
+        resultsHTML += `
+            <tr>
+                <td>${flexDienst.name}</td>
+                <td>Faktor: ${flexDienst.factor}</td>
+                <td>${flexDienst.totalHours} Stunden</td>
+                <td>${flexDienst.result.toFixed(2)}</td>
+            </tr>
+        `;
+    });
 
     resultsHTML += `</table>`;
     document.getElementById('results').innerHTML = resultsHTML;
